3e9e07c15a9286103dea097fd856354e7baf354a
Sarwar Zahan <szmithu1@gmail.com>
2022-10-15 01:43:54 +0600

    oh

--- resources/views/listings/edit.blade.php
+++ resources/views/listings/edit.blade.php
@@ -73,7 +73,7 @@
                 </label>
                 <input type="file" class="border border-gray-200 rounded p-2 w-full" name="logo" />
                 <img class="w-48 mr-6 mb-6"
-                    src="{{ $listing->logo ? asset($listing->logo) : asset('images/no-image.png') }}"
+                    src="{{ $listing->logo ? asset('storage/'.$listing->logo) : asset('images/no-image.png') }}"
                     alt="" />
                 @error('logo')
                     <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
