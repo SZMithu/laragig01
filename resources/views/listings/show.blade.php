233bafd9f0df6402303c97f45a92d0d5d6b652dc
Sarwar Zahan <szmithu1@gmail.com>
2022-10-14 23:56:32 +0600

    ok

--- resources/views/listings/show.blade.php
+++ resources/views/listings/show.blade.php
@@ -39,15 +39,5 @@
                 </div>
             </div>
         </x-card>
-        {{-- <x-card class="mt-2 p-2 flex space-x-6">
-            <a href="/listings/{{ $listing->id }}/edit">
-            <i class="fa-solid fa-pencil"> Edit</i>
-            </a>
-            <form action="/listings/{{$listing->id}}" method="POST">
-                @csrf
-                @method('DELETE')
-                <button class="text-red-500"><i class="fa-solid fa-trash"></i> Delete</button>
-            </form>
-        </x-card> --}}
     </div>
 </x-layout>
